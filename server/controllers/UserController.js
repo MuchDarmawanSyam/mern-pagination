28867643a5c942df8c1dc1d5ec991d495b51d06a
Much Darmawan Iriansyah Syam <mchdarmawan@gmail.com>
2023-01-31 14:08:53 +0900

    Membuat backend pagination

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,5 +1,40 @@
 import User from "../models/UserModel.js";
+import { Op } from "sequelize";
 
-export const getUsers = (req, res) => {
-
+export const getUsers = async(req, res) => {
+    const page = parseInt(req.query.page) || 0; // Berisi current page
+    const limit = parseInt(req.query.limit) || 10; // Berisi batas jumlah data per halaman
+    const search = req.query.search_query || ""; // Berisi string pencarian user
+    const offset = limit * page;
+    const totalRows = await User.count({
+        where: {
+            [Op.or]: [{name:{
+                [Op.like]: '%'+search+'%'
+            }}, {email:{
+                [Op.like]: '%'+search+'%'
+            }}]
+        }
+    });
+    const totalPage = Math.ceil(totalRows / limit);
+    const result = await User.findAll({
+        where: {
+            [Op.or]: [{name:{
+                [Op.like]: '%'+search+'%'
+            }}, {email:{
+                [Op.like]: '%'+search+'%'
+            }}]
+        },
+        offset: offset,
+        limit: limit,
+        order: [
+            ['id', 'DESC']
+        ]
+    });
+    res.json({
+        result: result,
+        page: page,
+        limit: limit,
+        totalRows: totalRows,
+        totalPage: totalPage
+    });
 }
--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ import UserRoutes from "./routes/UserRoutes.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(UserRoutes); 
-// Lanjut menit 15;45
+app.use(UserRoutes);
 
 app.listen('5000', ()=> console.log("Server running. . ."));
